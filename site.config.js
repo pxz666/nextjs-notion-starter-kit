11496febb3ab922462398bf0e70c24a25419699e
pengxz <pengxizhong@gmail.com>
2021-05-19 01:00:16 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '79712312d5e3434b8744f04a95f6cf0b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'PXZ',
+  domain: 'https://www.notion.so/PXZ-4cd0d869637f4e2f92cdea8cbfe1d4a5',
+  author: 'PXZ',
 
   // open graph metadata (optional)
   description: 'Example site description',
